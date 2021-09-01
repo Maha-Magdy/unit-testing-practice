853ea5bd60dbf1211083f196a806a4ce5c02c59d
Maha Magdy <mahamagdy3@gmail.com>
2021-09-01 15:27:34 +0200

    Add the conditions of the string passed, and its test case

--- string-length.test.js
+++ string-length.test.js
@@ -1,6 +1,10 @@
-const { expect } = require('@jest/globals');
-const stringLength = require('./string-length');
+const { expect } = require("@jest/globals");
+const stringLength = require("./string-length");
 
-test('length of Maha to equal 4', () => {
-  expect(stringLength('Maha')).toBe(4);
+test("length of Maha to equal 4", () => {
+  expect(stringLength("Maha")).toBe(4);
+});
+
+test('String passed doesn\'t met the conditions', () => {
+  expect(() => stringLength("Maha Magdy Abd-elaal")).toThrow('The string passed doesn\'t not met the conditions which are string should be at least 1 character long and not longer than 10 characters.');
 });
